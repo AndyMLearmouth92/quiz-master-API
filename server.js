8edd12bff7cf188e39825f5ce70017e6b26f5c69
Andy Learmouth <andymlearmouth@hotmail.com>
2023-10-02 17:41:44 +0100

    Created quiz question API.

---
+++ server.js
@@ -0,0 +1,550 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 3001;
+
+app.use(cors());
+
+const questions = [
+  {
+    id: 1,
+    questionText: "What does 'www' stand for in a website browser?",
+    answerOptions: [
+      { answerText: "World Wide Web", isCorrect: true },
+      { answerText: "Web World Wonders", isCorrect: false },
+      { answerText: "Wide Web Window", isCorrect: false },
+      { answerText: "World Website War", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    questionText: "How long is an Olympic swimming pool (in meters)?",
+    answerOptions: [
+      { answerText: "25 meters", isCorrect: false },
+      { answerText: "50 meters", isCorrect: true },
+      { answerText: "75 meters", isCorrect: false },
+      { answerText: "100 meters", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    questionText:
+      "What countries made up the original Axis powers in World War II?",
+    answerOptions: [
+      { answerText: "Germany, Italy, and Japan", isCorrect: true },
+      { answerText: "Germany, France, and Russia", isCorrect: false },
+      {
+        answerText: "United States, United Kingdom, and France",
+        isCorrect: false,
+      },
+      { answerText: "China, Russia, and Italy", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    questionText:
+      "Which country do cities of Perth, Adelade & Brisbane belong to?",
+    answerOptions: [
+      { answerText: "Canada", isCorrect: false },
+      { answerText: "Australia", isCorrect: true },
+      { answerText: "United States", isCorrect: false },
+      { answerText: "New Zealand", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    questionText: "What geometric shape is generally used for stop signs?",
+    answerOptions: [
+      { answerText: "Triangle", isCorrect: false },
+      { answerText: "Circle", isCorrect: false },
+      { answerText: "Octagon", isCorrect: true },
+      { answerText: "Square", isCorrect: false },
+    ],
+  },
+  {
+    id: 6,
+    questionText: "What is 'cynophobia'?",
+    answerOptions: [
+      { answerText: "Fear of spiders", isCorrect: false },
+      { answerText: "Fear of cats", isCorrect: false },
+      { answerText: "Fear of dogs", isCorrect: true },
+      { answerText: "Fear of heights", isCorrect: false },
+    ],
+  },
+  {
+    id: 7,
+    questionText: "What punctuation mark ends an imperative sentence?",
+    answerOptions: [
+      { answerText: "Comma", isCorrect: false },
+      { answerText: "Question mark", isCorrect: false },
+      { answerText: "Period or exclamation point", isCorrect: true },
+      { answerText: "Semicolon", isCorrect: false },
+    ],
+  },
+  {
+    id: 8,
+    questionText: "Who named the Pacific Ocean?",
+    answerOptions: [
+      { answerText: "Christopher Columbus", isCorrect: false },
+      { answerText: "Ferdinand Magellan", isCorrect: true },
+      { answerText: "Amerigo Vespucci", isCorrect: false },
+      { answerText: "Marco Polo", isCorrect: false },
+    ],
+  },
+  {
+    id: 9,
+    questionText: "How many languages are written from right to left?",
+    answerOptions: [
+      { answerText: "5", isCorrect: false },
+      { answerText: "10", isCorrect: false },
+      { answerText: "12", isCorrect: true },
+      { answerText: "15", isCorrect: false },
+    ],
+  },
+  {
+    id: 10,
+    questionText:
+      "How many countries still have Shilling as currency? Bonus point: Which countries?",
+    answerOptions: [
+      { answerText: "Three: Kenya, Uganda, and Tanzania", isCorrect: false },
+      {
+        answerText: "Four: Kenya, Uganda, Tanzania, and Somalia",
+        isCorrect: true,
+      },
+      {
+        answerText: "Five: Kenya, Uganda, Tanzania, Somalia, and Rwanda",
+        isCorrect: false,
+      },
+      { answerText: "Two: Kenya and Uganda", isCorrect: false },
+    ],
+  },
+  {
+    id: 11,
+    questionText: "What is the name of the man who launched eBay back in 1995?",
+    answerOptions: [
+      { answerText: "Jeff Bezos", isCorrect: false },
+      { answerText: "Pierre Omidyar", isCorrect: true },
+      { answerText: "Elon Musk", isCorrect: false },
+      { answerText: "Mark Zuckerberg", isCorrect: false },
+    ],
+  },
+  {
+    id: 12,
+    questionText:
+      "What is the name of the biggest technology company in South Korea?",
+    answerOptions: [
+      { answerText: "LG", isCorrect: false },
+      { answerText: "Hyundai", isCorrect: false },
+      { answerText: "Samsung", isCorrect: true },
+      { answerText: "Sony", isCorrect: false },
+    ],
+  },
+  {
+    id: 13,
+    questionText: "Which animal can be seen on the Porsche logo?",
+    answerOptions: [
+      { answerText: "Lion", isCorrect: false },
+      { answerText: "Horse", isCorrect: true },
+      { answerText: "Eagle", isCorrect: false },
+      { answerText: "Dragon", isCorrect: false },
+    ],
+  },
+  {
+    id: 14,
+    questionText:
+      "Which monarch officially made Valentine's Day a holiday in 1537?",
+    answerOptions: [
+      { answerText: "Queen Victoria", isCorrect: false },
+      { answerText: "King Henry VIII", isCorrect: true },
+      { answerText: "Queen Elizabeth I", isCorrect: false },
+      { answerText: "King George III", isCorrect: false },
+    ],
+  },
+  {
+    id: 15,
+    questionText: "Who was the first woman to win a Nobel Prize (in 1903)?",
+    answerOptions: [
+      { answerText: "Marie Curie", isCorrect: true },
+      { answerText: "Rosa Parks", isCorrect: false },
+      { answerText: "Amelia Earhart", isCorrect: false },
+      { answerText: "Jane Goodall", isCorrect: false },
+    ],
+  },
+  {
+    id: 16,
+    questionText: "The first dictionary was written by?",
+    answerOptions: [
+      { answerText: "Noah Webster", isCorrect: false },
+      { answerText: "Samuel Johnson", isCorrect: true },
+      { answerText: "Ambrose Bierce", isCorrect: false },
+      { answerText: "William Shakespeare", isCorrect: false },
+    ],
+  },
+  {
+    id: 17,
+    questionText: "Worship of Krishna is observed by which Religious Faith?",
+    answerOptions: [
+      { answerText: "Christianity", isCorrect: false },
+      { answerText: "Islam", isCorrect: false },
+      { answerText: "Hinduism", isCorrect: true },
+      { answerText: "Buddhism", isCorrect: false },
+    ],
+  },
+  {
+    id: 18,
+    questionText: "What is the name of the World's largest ocean?",
+    answerOptions: [
+      { answerText: "Atlantic Ocean", isCorrect: false },
+      { answerText: "Indian Ocean", isCorrect: false },
+      { answerText: "Arctic Ocean", isCorrect: false },
+      { answerText: "Pacific Ocean", isCorrect: true },
+    ],
+  },
+  {
+    id: 19,
+    questionText:
+      "Demolition of the Berlin wall separating East and West Germany began in what year?",
+    answerOptions: [
+      { answerText: "1987", isCorrect: false },
+      { answerText: "1988", isCorrect: false },
+      { answerText: "1989", isCorrect: true },
+      { answerText: "1990", isCorrect: false },
+    ],
+  },
+  {
+    id: 20,
+    questionText: "What is the romanized Arabic word for 'moon'?",
+    answerOptions: [
+      { answerText: "Shams", isCorrect: false },
+      { answerText: "Kawkab", isCorrect: false },
+      { answerText: "Qamar", isCorrect: true },
+      { answerText: "Najm", isCorrect: false },
+    ],
+  },
+  {
+    id: 21,
+    questionText:
+      "Who was the first woman pilot to fly solo across the Atlantic?",
+    answerOptions: [
+      { answerText: "Bessie Coleman", isCorrect: false },
+      { answerText: "Sally Ride", isCorrect: false },
+      { answerText: "Valentina Tereshkova", isCorrect: false },
+      { answerText: "Amelia Earhart", isCorrect: true },
+    ],
+  },
+  {
+    id: 22,
+    questionText: "What is the rarest M&M color?",
+    answerOptions: [
+      { answerText: "Red", isCorrect: false },
+      { answerText: "Blue", isCorrect: false },
+      { answerText: "Green", isCorrect: false },
+      { answerText: "Brown", isCorrect: true },
+    ],
+  },
+  {
+    id: 23,
+    questionText: "What is the common name for dried plums?",
+    answerOptions: [
+      { answerText: "Raisins", isCorrect: false },
+      { answerText: "Prunes", isCorrect: true },
+      { answerText: "Apricots", isCorrect: false },
+      { answerText: "Dates", isCorrect: false },
+    ],
+  },
+  {
+    id: 24,
+    questionText: "Which country consumes the most chocolate per capita?",
+    answerOptions: [
+      { answerText: "Switzerland", isCorrect: true },
+      { answerText: "Belgium", isCorrect: false },
+      { answerText: "USA", isCorrect: false },
+      { answerText: "Germany", isCorrect: false },
+    ],
+  },
+  {
+    id: 25,
+    questionText:
+      "What is the name given to Indian food cooked over charcoal in a clay oven?",
+    answerOptions: [
+      { answerText: "Tandoori", isCorrect: true },
+      { answerText: "Sushi", isCorrect: false },
+      { answerText: "Barbecue", isCorrect: false },
+      { answerText: "Fondue", isCorrect: false },
+    ],
+  },
+  {
+    id: 26,
+    questionText: "What was the first soft drink in space?",
+    answerOptions: [
+      { answerText: "Pepsi", isCorrect: false },
+      { answerText: "Coca-Cola", isCorrect: true },
+      { answerText: "Sprite", isCorrect: false },
+      { answerText: "Dr. Pepper", isCorrect: false },
+    ],
+  },
+  {
+    id: 27,
+    questionText: "What is the most consumed manufactured drink in the world?",
+    answerOptions: [
+      { answerText: "Coffee", isCorrect: false },
+      { answerText: "Soda", isCorrect: false },
+      { answerText: "Tea", isCorrect: true },
+      { answerText: "Juice", isCorrect: false },
+    ],
+  },
+  {
+    id: 28,
+    questionText: "Which is the only edible food that never goes bad?",
+    answerOptions: [
+      { answerText: "Bread", isCorrect: false },
+      { answerText: "Milk", isCorrect: false },
+      { answerText: "Honey", isCorrect: true },
+      { answerText: "Eggs", isCorrect: false },
+    ],
+  },
+  {
+    id: 29,
+    questionText: "Which country invented ice cream?",
+    answerOptions: [
+      { answerText: "Italy", isCorrect: false },
+      { answerText: "France", isCorrect: false },
+      { answerText: "China", isCorrect: true },
+      { answerText: "United States", isCorrect: false },
+    ],
+  },
+  {
+    id: 30,
+    questionText:
+      '"Hendrick’s," "Larios," and "Seagram’s" are some of the best-selling brands of which spirit?',
+    answerOptions: [
+      { answerText: "Vodka", isCorrect: false },
+      { answerText: "Rum", isCorrect: false },
+      { answerText: "Gin", isCorrect: true },
+      { answerText: "Whiskey", isCorrect: false },
+    ],
+  },
+  {
+    id: 31,
+    questionText: "From which country does Gouda cheese originate?",
+    answerOptions: [
+      { answerText: "France", isCorrect: false },
+      { answerText: "Italy", isCorrect: false },
+      { answerText: "Netherlands", isCorrect: true },
+      { answerText: "Switzerland", isCorrect: false },
+    ],
+  },
+  {
+    id: 32,
+    questionText: "What was the first toy to be advertised on television?",
+    answerOptions: [
+      { answerText: "Barbie", isCorrect: false },
+      { answerText: "Lego", isCorrect: false },
+      { answerText: "Mr. Potato Head from Toy Story", isCorrect: true },
+      { answerText: "Hot Wheels", isCorrect: false },
+    ],
+  },
+  {
+    id: 33,
+    questionText:
+      "What was the first feature-length animated movie ever released? (Hint: It was a Disney Movie)",
+    answerOptions: [
+      { answerText: "Aladdin", isCorrect: false },
+      { answerText: "Cinderella", isCorrect: false },
+
+      { answerText: "Snow White and the Seven Dwarfs", isCorrect: true },
+      { answerText: "The Lion King", isCorrect: false },
+    ],
+  },
+  {
+    id: 34,
+    questionText:
+      "What TV series showed the first interracial kiss on American network television?",
+    answerOptions: [
+      { answerText: "Friends", isCorrect: false },
+      { answerText: "The Simpsons", isCorrect: false },
+      { answerText: "Star Trek", isCorrect: true },
+      { answerText: "The Fresh Prince of Bel-Air", isCorrect: false },
+    ],
+  },
+  {
+    id: 35,
+    questionText:
+      "What were the four main characters' names in the TV series 'Golden Girls' that ran from 1985-1992?",
+    answerOptions: [
+      { answerText: "Monica, Rachel, Phoebe, and Ross", isCorrect: false },
+      { answerText: "Dorothy, Rose, Blanche, and Sophia", isCorrect: true },
+      { answerText: "Lucy, Ethel, Ricky, and Fred", isCorrect: false },
+      { answerText: "Mary, Rhoda, Lou, and Murray", isCorrect: false },
+    ],
+  },
+  {
+    id: 36,
+    questionText: "Which Disney Princess talks to the most animals?",
+    answerOptions: [
+      { answerText: "Ariel (The Little Mermaid)", isCorrect: false },
+      { answerText: "Belle (Beauty and the Beast)", isCorrect: false },
+      { answerText: "Cinderella", isCorrect: false },
+      { answerText: "Snow White", isCorrect: true },
+    ],
+  },
+  {
+    id: 37,
+    questionText: "What awards has an EGOT winner won?",
+    answerOptions: [
+      { answerText: "An Emmy, Grammy, and an Oscar", isCorrect: false },
+      { answerText: "An Emmy, Grammy, Oscar, and a Tony", isCorrect: true },
+      {
+        answerText: "An Emmy, Golden Globe, Oscar, and a Tony",
+        isCorrect: false,
+      },
+      { answerText: "An Emmy, Tony, and a Pulitzer", isCorrect: false },
+    ],
+  },
+  {
+    id: 38,
+    questionText: "Which member of the Beatles married Yoko Ono?",
+    answerOptions: [
+      { answerText: "Paul McCartney", isCorrect: false },
+      { answerText: "George Harrison", isCorrect: false },
+      { answerText: "Ringo Starr", isCorrect: false },
+      { answerText: "John Lennon", isCorrect: true },
+    ],
+  },
+  {
+    id: 39,
+    questionText: "What are the names of Cinderella’s stepsisters?",
+    answerOptions: [
+      { answerText: "Anastasia and Drizella", isCorrect: true },
+      { answerText: "Lucy and Ethel", isCorrect: false },
+      { answerText: "Mary and Rhoda", isCorrect: false },
+      { answerText: "Thelma and Louise", isCorrect: false },
+    ],
+  },
+  {
+    id: 40,
+    questionText: "What famous US festival hosted over 350,000 fans in 1969?",
+    answerOptions: [
+      { answerText: "Coachella", isCorrect: false },
+      { answerText: "Woodstock", isCorrect: true },
+      { answerText: "Lollapalooza", isCorrect: false },
+      { answerText: "Burning Man", isCorrect: false },
+    ],
+  },
+  {
+    id: 41,
+    questionText: "The biggest selling music single of all time is?",
+    answerOptions: [
+      { answerText: "Bohemian Rhapsody", isCorrect: false },
+      { answerText: "Candle in the Wind (1997)", isCorrect: true },
+      { answerText: "Billie Jean", isCorrect: false },
+      { answerText: "Shape of You", isCorrect: false },
+    ],
+  },
+  {
+    id: 42,
+    questionText:
+      "When Walt Disney was a child, which character did he play in his school function?",
+    answerOptions: [
+      { answerText: "Mickey Mouse", isCorrect: false },
+      { answerText: "Donald Duck", isCorrect: false },
+      { answerText: "Peter Pan", isCorrect: true },
+      { answerText: "Snow White", isCorrect: false },
+    ],
+  },
+  {
+    id: 43,
+    questionText:
+      "Which two countries have not missed one of the modern-day Olympics?",
+    answerOptions: [
+      { answerText: "United States and Canada", isCorrect: false },
+      { answerText: "Greece and Australia", isCorrect: true },
+      { answerText: "Russia and China", isCorrect: false },
+      { answerText: "Brazil and Germany", isCorrect: false },
+    ],
+  },
+  {
+    id: 44,
+    questionText:
+      "Which is the only American Football team to go a whole season undefeated, including the Super Bowl?",
+    answerOptions: [
+      { answerText: "Dallas Cowboys", isCorrect: false },
+      { answerText: "San Francisco 49ers", isCorrect: false },
+      { answerText: "New England Patriots", isCorrect: false },
+      { answerText: "The Miami Dolphins (1972)", isCorrect: true },
+    ],
+  },
+  {
+    id: 45,
+    questionText:
+      "Who has won more tennis grand slam titles, Venus Williams or Serena Williams?",
+    answerOptions: [
+      { answerText: "Venus Williams", isCorrect: false },
+      { answerText: "Serena Williams", isCorrect: true },
+      { answerText: "They have the same number of titles", isCorrect: false },
+      { answerText: "None of the above", isCorrect: false },
+    ],
+  },
+  {
+    id: 46,
+    questionText: "Which Basketball team has completed two threepeats?",
+    answerOptions: [
+      { answerText: "Los Angeles Lakers", isCorrect: false },
+      { answerText: "Boston Celtics", isCorrect: false },
+      { answerText: "Chicago Bulls", isCorrect: true },
+      { answerText: "Golden State Warriors", isCorrect: false },
+    ],
+  },
+  {
+    id: 47,
+    questionText: "Which Former NBA Player Was Nicknamed 'Agent Zero'?",
+    answerOptions: [
+      { answerText: "LeBron James", isCorrect: false },
+      { answerText: "Stephen Curry", isCorrect: false },
+      { answerText: "Kobe Bryant", isCorrect: false },
+      { answerText: "Gilbert Arenas", isCorrect: true },
+    ],
+  },
+  {
+    id: 48,
+    questionText: "What sport is dubbed the 'king of sports'?",
+    answerOptions: [
+      { answerText: "Soccer", isCorrect: true },
+      { answerText: "American Football", isCorrect: false },
+      { answerText: "Cricket", isCorrect: false },
+      { answerText: "Tennis", isCorrect: false },
+    ],
+  },
+  {
+    id: 49,
+    questionText: "Which country has hosted the most Olympic Games?",
+    answerOptions: [
+      { answerText: "United States", isCorrect: true },
+      { answerText: "France", isCorrect: false },
+      { answerText: "Japan", isCorrect: false },
+      { answerText: "Germany", isCorrect: false },
+    ],
+  },
+  {
+    id: 50,
+    questionText: "What is the national sport of Japan?",
+    answerOptions: [
+      { answerText: "Sumo Wrestling", isCorrect: true },
+      { answerText: "Karate", isCorrect: false },
+      { answerText: "Judo", isCorrect: false },
+      { answerText: "Kendo", isCorrect: false },
+    ],
+  },
+];
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/questions", (request, response) => {
+  response.json(questions);
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
